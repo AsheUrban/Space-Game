8f18fc884a9f9f3b9cf4215172348fe94dd1fc57
Justin Peters <JMarvel@gmail.com>
2022-07-05 14:54:56 -0700

    add scrolling background and character image.
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/scripts.js
+++ js/scripts.js
@@ -44,32 +44,68 @@ window.addEventListener("load", function(){
       this.y = this.gameHeight - this.height;
       this.image = document.getElementById("playerImage");
       this.speed= 0;
+      this.vy = 0;
+      this.weight = 1;
     }
+
     draw(context){
       context.fillStyle = "white";
       context.fillRect(this.x, this.y, this.width, this.height);
       context.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
+
     update(input){
-      //horizontal movement
-      this.x += this.speed;
+      
       if (input.keys.indexOf("ArrowRight") > -1) {
         this.speed = 5;
       } else if (input.keys.indexOf("ArrowLeft") > -1) {
         this.speed = -5;
-      } else {
+      } else if (input.keys.indexOf('ArrowUp') > -1 && this.onGround()) {
+        this.vy -= 30;
+      } 
+      else {
         this.speed = 0;
       }
+     
+      //horizontal movement
+      this.x += this.speed;
       if (this.x < 0) this.x = 0;
       else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width
+      //vertical movement
+      this.y += this.vy;
+      if (!this.onGround()){
+        this.vy += this.weight;
+      } else {
+        this.vy = 0;
+      }
+      if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height
     }
-
+    onGround() {
+      return this.y >= this.gameHeight - this.height;
+    }
   }
 
   // Background environment for Player && Enemy objects
 
   class Background {
-
+    constructor(gameWidth, gameHeight){
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.image = document.getElementById('backgroundImage');
+      this.x = 0;
+      this.y = 1; 
+      this.width = 1200;
+      this.height = 1000;
+      this.speed = 2;
+    }
+    draw(context){
+      context.drawImage(this.image, this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    }
+    update(){
+      this.x -= this.speed;
+      if (this.x < 0 - this.width) this.x = 0;
+    }
   }
 
   //Enemy object && function
@@ -90,13 +126,16 @@ window.addEventListener("load", function(){
 
   const input = new ControlInput();
   const player = new Player(canvas.width, canvas.height);
+  const background = new Background(canvas.width, canvas.height);
   
 
 
   // Perpetual drawing && animation loop
 
   function animationLoop(){
-    ctx.clearRect(0,0,canvas.width, canvas.height)
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    background.draw(ctx);
+    background.update();
     player.draw(ctx);
     player.update(input);
     requestAnimationFrame(animationLoop);
