9bfed712d0c580b947354a171b22903d46bb7e60
Justin Peters <JMarvel@gmail.com>
2022-07-05 13:51:56 -0700

    add input functionality to move block the right with arrow right
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-window.addEventListener('load', function(){
-  const canvas = document.getElementById('canvas-1');
-  const ctx = canvas.getContext('2d');
+window.addEventListener("load", function(){
+  const canvas = document.getElementById("canvas-1");
+  const ctx = canvas.getContext("2d");
   canvas.width = 800;
   canvas.height = 720;
 
@@ -9,35 +9,54 @@ window.addEventListener('load', function(){
   class ControlInput {
     constructor() {
       this.keys = [];
-      window.addEventListener('keydown', function(e){
-        console.log(e);
+      window.addEventListener("keydown", direction => {
+        if ((   direction.key === "ArrowDown" ||
+                direction.key === "ArrowUp" ||
+                direction.key === "ArrowLeft" ||
+                direction.key === "ArrowRight")
+                && this.keys.indexOf(direction.key) === -1) {
+          this.keys.push(direction.key);
+        }
+        console.log(direction.key, this.keys);
       });
-      // window.addEventListener("keydown", e => {
-      //   if ((   e.key === "ArrowDown" ||
-      //           e.key === "ArrowUp" ||
-      //           e.key === "ArrowLeft" ||
-      //           e.key === "ArrowRight")
-      //           && this.keys.indexOf(e.key) === -1) {
-      //     this.keys.push(e.key);
-      //   }
-      //   console.log(e.key, this.keys);
-      // });
 
-      // window.addEventListener("keyup", e => {
-      //   if (    e.key === "ArrowDown" ||
-      //           e.key === "ArrowUp" ||
-      //           e.key === "ArrowLeft" ||
-      //           e.key === "ArrowRight") {
-      //     this.key.splice(this.keys.indexOf(e.key), 1);
-      //   }
-      //   console.log(e.key, this.keys);
-      // });
+      window.addEventListener("keyup", direction => {
+        if (    direction.key === "ArrowDown" ||
+                direction.key === "ArrowUp" ||
+                direction.key === "ArrowLeft" ||
+                direction.key === "ArrowRight") {
+          this.keys.splice(this.keys.indexOf(direction.key), 1);
+        }
+        // console.log(direction.key, this.keys);
+      });
     }
   }
 
   //Player object && function
 
   class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = this.gameHeight - this.height;
+      this.speed= 0;
+    }
+    draw(context){
+      context.fillStyle = "white";
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    update(input){
+      //horizontal movement
+      this.x += this.speed;
+      if (input.keys.indexOf("ArrowRight") > -1) {
+        this.speed = 5;
+      } else {
+        this.speed = 0;
+      }
+    }
 
   }
 
@@ -64,10 +83,18 @@ window.addEventListener('load', function(){
   }
 
   const input = new ControlInput();
+  const player = new Player(canvas.width, canvas.height);
+  
+
 
   // Perpetual drawing && animation loop
 
   function animationLoop(){
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+    player.draw(ctx);
+    player.update(input);
+    requestAnimationFrame(animationLoop);
 
   }
+  animationLoop();
 })
